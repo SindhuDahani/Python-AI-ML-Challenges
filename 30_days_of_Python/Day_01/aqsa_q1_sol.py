c7ab5a6343e8b113d6242fdb9ec2ace6160edbab
Aqsazubair24 <aqsazuabir480@gmail.com>
2026-07-04 20:28:26 +0500

    Add solutions for Q1, Q2, and Q3; create new file for Q4, Q5, and Q6 prompts

--- 30_days_of_Python/Day_01/aqsa_q1_sol.py
+++ 30_days_of_Python/Day_01/aqsa_q1_sol.py
@@ -1,3 +1,29 @@
-Q1. Write a Python program to swap two variables. 
-Q2. Take user input and display it back to the user. 
-Q3. Write a program to check if a number is even or odd.
+#Q1. Write a Python program to swap two variables. 
+#Q2. Take user input and display it back to the user. 
+#Q3. Write a program to check if a number is even or odd.
+
+                #solution
+#Q1. Write a Python program to swap two variables. 
+# solution_1
+# x = 5
+# y = 10
+# print(x, y)
+# x, y = y, x
+# print(x, y)
+
+
+#Q2. Take user input and display it back to the user. 
+#solution_2
+# user_input=(input("HELLO, PLEASE TYPE YOUR NAME:"))
+# print(f"HELLO, {user_input}!")
+
+
+
+
+#Q3. Write a program to check if a number is even or odd.
+# solution_3
+# number = int(input("Enter a number: "))
+# if number % 2 == 0:
+#     print(f"{number} is even.")
+# else:
+#     print(f"{number} is odd.")
